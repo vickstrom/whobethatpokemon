8029a743c7575921c992d3144a5b153e5405e44b
Eric Vickström <ericvi@kth.se>
2021-11-30 16:37:19 +0100

    feature: contours of pikachut

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Jimp from 'jimp';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [imgData, setImageData] = useState()
+
+  useEffect(() => {
+    Jimp.read("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png").then(image => {
+
+      image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+          image.bitmap.data[idx] = 86;
+          image.bitmap.data[idx + 1] = 101;
+          image.bitmap.data[idx + 2] = 115;
+          image.bitmap.data[idx + 3] = image.bitmap.data[idx + 3];
+      });
+      image.getBase64Async(Jimp.MIME_PNG).then(newImage => { 
+        let tag = document.createElement("img");
+        setImageData(newImage);
+      })
+    })  
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" />
+        <img src={imgData}  />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
