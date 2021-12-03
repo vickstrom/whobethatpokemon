a9a79b331cfd150f9e6555d04771e23e8d8ed393
wizkas0 <kalle.meurman@hotmail.com>
2021-12-03 15:48:41 +0100

    Made additions to  model

--- app/src/model/pokeModel.js
+++ app/src/model/pokeModel.js
@@ -1,5 +1,5 @@
 class PokeModel{
-    constructor(trainers=[], rooms=[])
+    constructor(trainers={}, rooms={})
     {
         this.setTrainers(trainers);
         this.setRooms(rooms);
@@ -14,20 +14,48 @@ class PokeModel{
     }
 
     addNewTrainer(trainer){
-        this.trainers = [...this.trainers].push(trainer)
+        this.trainers = [...this.trainers].push(trainer);
+        this.notifyObservers();
+    }
+    
+    updateLeaderBoard(){
+        const leaderBoard = Object.values(this.trainers).map(trainer => {return {name: trainer.name, points: trainer.points}}).sort(compareScore);
+        this.leaderBoard = leaderBoard;
+        this.notifyObservers;
     }
 
     addTrainerToRoom(trainer, roomId){
-        const trainer = this.trainers.find(trainer => trainer.id === trainerId)
-        const room = this.rooms.find(room => room.id === roomId)
-        const trainers = [...room.trainers].push(trainer)
-        const newRoom = Object.assign
-    
-        this.rooms = [...this.rooms].filter(room => room.id === id)
+        const newRooms = [...this.rooms];
+        newRooms[roomId].trainers[trainer.id] = trainer;
+        newRooms[roomId].numberOfTrainers ++;
+        this.rooms = newRooms;
+        this.notifyObservers();
     }
-
 
     addRoom(room){
-        this.rooms = [...this.rooms].push(room)
+        this.rooms = [...this.rooms].push(room);
+        this.notifyObservers();
     }
+
+    addObserver(callback){
+        this.observers = [this.observers].push(callback);
+    }
+    removeObserver(callback){
+        this.observers = [this.observers].filter(observer => observer != callback);
+    }
+    notifyObservers(){
+        this.observers.forEach(cb=> cb(this));
+    }
+}
+
+function compareScore(a,b){
+    if(a.points < b.points)
+    return -1;
+ else if(a.points > b.points)
+     return 1;
+ if(a.name < b.name)
+     return -1;
+ else if(a.name > b.name)
+     return 1;
+ throw console.error("this is bad");
 }
