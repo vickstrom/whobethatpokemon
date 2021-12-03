292dfd0c6dfca2f86f43103cd4c95c5e7d652e6c
skarn <sebbe.karn@gmail.com>
2021-12-03 21:37:13 +0100

    fixade färger och alignade texten snyggare i Play

--- app/src/components/view/leaderboard/index.js
+++ app/src/components/view/leaderboard/index.js
@@ -8,10 +8,11 @@ export default function LeaderBoardView(props) {
                     return (<tr>
                         <td>{index + 1}.</td>
                         <td>{trainer.name}</td>
-                        <td>{trainer.points}</td>
+                        <td className={'right'}>{trainer.points}</td>
                     </tr>)
                 })}
             </table>
         </div>
     )
 }
+
