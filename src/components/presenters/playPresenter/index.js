959677983bb00c7857a3ce826a2269d8f8b201cf
Eric Vickström <ericvi@kth.se>
2021-12-06 14:35:24 +0100

    feature: play view

--- src/components/presenters/playPresenter/index.js
+++ src/components/presenters/playPresenter/index.js
@@ -15,32 +15,34 @@ export default function PlayPresenter(props) {
     }, []);
 
     return (
-        <div>
-            <div className={'mainView'}>
-                <WhoPokemonView image={currentRoom.getCurrentImage() || 'http://www.csc.kth.se/~cristi/loading.gif'} />
-                <LeaderBoardView leaderboard={
-                    [{name: "Kalle", points: 1337}, {name: "Kungen", points: -1}]
-                } />
+        <div className={"play"}>
+            <div className={'play-split'}>
+                <div className={'mainView'}>
+                    <WhoPokemonView image={currentRoom.getCurrentImage() || 'http://www.csc.kth.se/~cristi/loading.gif'} />
+                    <LeaderBoardView leaderboard={
+                        [{name: "Kalle", points: 1337}, {name: "Kungen", points: -1}]
+                    } />
+                </div>
+                <QuizAlternativesView 
+                    alternatives={currentRoom.getAlternativesNames()}
+                    answerClasses={answerClasses}
+                    onGuess={(name, index) => {
+                        if (waiting) return;
+                        setWaiting(true);
+                        let [correct, correctName] = props.model.guess(props.model.currentRoomId, name);
+                        let correctIndex = currentRoom.getAlternativesNames().indexOf(correctName);
+                        var newAnswerClasses = [...answerClasses];
+                        newAnswerClasses[index] = correct ? "correct" : "incorrect";
+                        // Correct answer should always be green
+                        newAnswerClasses[correctIndex] = "correct";
+                        setAnswerClasses(newAnswerClasses);
+                        setTimeout(async () => {
+                            await props.model.newRound(props.model.currentRoomId);
+                            setAnswerClasses(defaultAnswerClasses);
+                            setWaiting(false);
+                        }, 2000);
+                    }}/>
             </div>
-            <QuizAlternativesView 
-                alternatives={currentRoom.getAlternativesNames()}
-                answerClasses={answerClasses}
-                onGuess={(name, index) => {
-                    if (waiting) return;
-                    setWaiting(true);
-                    let [correct, correctName] = props.model.guess(props.model.currentRoomId, name);
-                    let correctIndex = currentRoom.getAlternativesNames().indexOf(correctName);
-                    var newAnswerClasses = [...answerClasses];
-                    newAnswerClasses[index] = correct ? "correct" : "incorrect";
-                    // Correct answer should always be green
-                    newAnswerClasses[correctIndex] = "correct";
-                    setAnswerClasses(newAnswerClasses);
-                    setTimeout(async () => {
-                        await props.model.newRound(props.model.currentRoomId);
-                        setAnswerClasses(defaultAnswerClasses);
-                        setWaiting(false);
-                    }, 2000);
-                }}/>
         </div>
     )
 }
