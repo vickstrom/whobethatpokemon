0bf1a8af45c842e8960c19ffaceffb0729132f09
wizkas0 <kalle.meurman@hotmail.com>
2021-12-14 21:39:17 +0100

    You can now pick an avatar

--- src/components/presenters/registerPresenter/index.js
+++ src/components/presenters/registerPresenter/index.js
@@ -1,18 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import pokeAPI from "../../../utils/pokeapi";
+import RegisterView from "../../view/register";
 
 export default function RegisterPresenter(props) {
+    const randomIDs = Array.from({length: 20}, () => Math.floor(Math.random() * 898 + 1));
     const [displayName, setDisplayName] = useState("");
     const navigate = useNavigate();
-    return (
+    const [pokemen, setPokemen] = useState(null);
+    const [pokemonID, setPokemonID] = useState(null);
+
+    useEffect(() => {
+        Promise.all(randomIDs.map(id => {return pokeAPI.getPokemon(id)}))
+        .then(pokemen => {console.log(pokemen);setPokemen(pokemen)})
+        .catch(error => {console.log(error)})
+    }, []);
+
+    return ( pokemen ?
         <div>
-            <input type="text" onChange={e => setDisplayName(e.target.value)} />
-            <button onClick={() => {
-                props.model.databaseHandler.setAccountDetails(displayName).then(() => {
+            <RegisterView 
+            pokemon={pokemen.map(e => e.data)}
+            setPokemon={id => setPokemonID(id)}
+            setDisplayName={name => setDisplayName(name)}
+            disabled= {pokemonID && (displayName.length > 3)? false :true}
+            createAccount={() => {
+                props.model.databaseHandler.setAccountDetails(displayName, pokemonID).then(() => {
                     navigate('/rooms');
                 });
-                console.log("register")
-
-            }}>Register</button>
+                console.log("register")}}
+            />
         </div>
+        :null
     ) }
