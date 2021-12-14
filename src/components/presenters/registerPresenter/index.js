bd7509c0290ec00967dd67356a97f8c151e52572
wizkas0 <kalle.meurman@hotmail.com>
2021-12-14 21:49:06 +0100

    fixed

--- src/components/presenters/registerPresenter/index.js
+++ src/components/presenters/registerPresenter/index.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import pokeAPI from "../../../utils/pokeapi";
 import RegisterView from "../../view/register";
 import { useNavigate, useSearchParams } from 'react-router-dom';
