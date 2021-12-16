3cf11c29275e0cda2f850cea385b5d8f51b852a8
Eric Vickström <ericvi@kth.se>
2021-12-16 17:14:56 +0100

    refactor: loading img

--- src/components/view/spinner/index.js
+++ src/components/view/spinner/index.js
@@ -1,3 +1,3 @@
 export default function Spinner() {
-    return (<img src="https://www.aopcatholicschools.org/wp-content/uploads/2017/09/fidget-spinner.gif"></img>)
+    return (<img src="https://i.pinimg.com/originals/8b/22/c2/8b22c2b446f9f539693c305d7e599ef6.gif"></img>)
 }
