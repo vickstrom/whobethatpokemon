32e06ca8c1d8a0817da3b6b7ad045ca75f8a46c7
wizkas0 <kalle.meurman@hotmail.com>
2021-12-19 16:59:51 +0100

    changed stupid text

--- src/components/view/nameSelector/index.js
+++ src/components/view/nameSelector/index.js
@@ -7,7 +7,7 @@ export default function NameSelectorView(props) {
     return (
         <div className={'nameSelector'}>
             <Window>
-                <p>To register, name with at least 3 characters and an avatar</p>
+                <p>To register, pick a name with at least three characters and an avatar!</p>
                 <h3>Choose your trainer name</h3>
                 <div>
                     <Input placeholder={props.displayName} maxLength={10} type="text" onChange={e => props.setDisplayName(e.target.value)} />
